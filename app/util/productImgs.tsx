ac61c7810381fb72017aad5565de68182ce26933
Andrea Hsu <andrea.hsu1030@gmail.com>
2024-12-05 19:29:50 +0800

    first commit

---
+++ app/util/productImgs.tsx
@@ -0,0 +1,23 @@
+import photoA from '../../public/cards/1.jpeg'
+import photoB from '../../public/cards/2.jpeg'
+import photoC from '../../public/cards/3.jpeg'
+
+const imageList = [
+  {
+    title: '妙花種子',
+    'sub-title': '妙妙妙',
+    href: photoA
+  },
+  {
+    title: '傑尼龜',
+    'sub-title': '傑尼 傑尼',
+    href: photoB
+  },
+  {
+    title: '卡比獸',
+    'sub-title': '卡比卡比',
+    href: photoC
+  }
+]
+
+export default imageList
